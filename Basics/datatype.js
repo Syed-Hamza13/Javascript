14170f1fefce4a59aa530cf7bfede1e8e4590ea0
Syed Hamza <134732974+syed13hamza@users.noreply.github.com>
2025-03-07 23:09:11 +0530

    Update datatype.js

--- Basics/datatype.js
+++ Basics/datatype.js
@@ -25,3 +25,55 @@ console.log(state);
 console.log(typeof state)
 console.log(typeof undefined)
 
+
+// Datatype conversion
+console.log("Datatype Convertion in interger -------------------");
+
+let score = false // assign diiferent values of different datatypes to check conversion
+
+console.log("before conversion ------------");
+
+console.log(score);
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)  // converting into Number 
+
+console.log("After conversion ------------");
+
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+console.log("convertion in boolean -----------");
+
+let isLogged = 0 // assign different values to check conversion
+
+console.log("before conversion ------------");
+console.log(isLogged);
+console.log(typeof isLogged);
+
+let booleanIsLogged = Boolean(isLogged) // converting into boolean 
+
+console.log("After conversion --------------");
+console.log(booleanIsLogged)
+console.log(typeof booleanIsLogged)
+
+// 1 => true,   0 => false
+
+// convertion into String -------
+
+let someNumber = 58794 // assign different values to check conversion
+
+console.log("before conversion ------------");
+console.log(someNumber);
+console.log(typeof someNumber);
+
+
+let stringNumber = String(someNumber) // converting into string
+
+console.log("After conversion --------------");
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
